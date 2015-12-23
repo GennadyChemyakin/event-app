37a3d9307bdfac5f7eeec57f7aa3cfd92d973a0a
GennadyChemyakin <gmeinpost@gmail.com>
2015-12-23 15:17:15 +0300

    ea-13 remove unused annotation from controller, add logback dependency to web module

--- event-app-web/src/main/java/com/epam/eventappweb/controller/EventDetailController.java
+++ event-app-web/src/main/java/com/epam/eventappweb/controller/EventDetailController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
@@ -17,17 +16,10 @@ import java.util.Optional;
  * otherwise return ResponseEntity with HttpStatus code 404
  */
 @RestController
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class EventDetailController {
-
 
     @Autowired
     private EventDAO eventDAO;
-
-//    @Autowired
-//    public EventDetailController(EventDAO eventDAO) {
-//        this.eventDAO = eventDAO;
-//    }
 
     @RequestMapping("/event/{id}")
     public ResponseEntity<Event> getEventDetail(@PathVariable("id") int eventId) {
