72ecb733de35dd936021133c17f34edb451f07aa
GennadyChemyakin <gmeinpost@gmail.com>
2015-12-30 17:55:26 +0300

    ea-14 add logs, change logback config, add logback-test.xml

--- event-app-service/src/main/java/com/epam/eventapp/service/service/impl/EventServiceImpl.java
+++ event-app-service/src/main/java/com/epam/eventapp/service/service/impl/EventServiceImpl.java
@@ -3,6 +3,8 @@ package com.epam.eventapp.service.service.impl;
 import com.epam.eventapp.service.dao.EventDAO;
 import com.epam.eventapp.service.domain.Event;
 import com.epam.eventapp.service.service.EventService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +16,19 @@ import java.util.Optional;
 @Service("EventService")
 public class EventServiceImpl implements EventService {
 
+    private static final Logger logger = LoggerFactory.getLogger(EventServiceImpl.class);
+
     @Autowired
     private EventDAO eventDAO;
 
     @Override
     public Optional<Event> findById(int id) {
-        return eventDAO.findById(id);
+        logger.debug("findById from EventServiceImpl method has been started: id =" + id);
+        Optional<Event> event = eventDAO.findById(id);
+        logger.debug("findById from EventServiceImpl method has been finished. Return Optional<event>: " +
+                "event.isPresent() = " + event.isPresent() +
+                "; event id = " + event.map(Event::getId).map(String::valueOf).orElse("null") +
+                "; event name = " + event.map(Event::getName).orElse("null"));
+        return event;
     }
 }
--- event-app-web/src/main/java/com/epam/eventappweb/controller/EventDetailController.java
+++ event-app-web/src/main/java/com/epam/eventappweb/controller/EventDetailController.java
@@ -2,6 +2,8 @@ package com.epam.eventappweb.controller;
 
 import com.epam.eventapp.service.domain.Event;
 import com.epam.eventapp.service.service.EventService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +20,20 @@ import java.util.Optional;
 @RestController
 public class EventDetailController {
 
+    private static final Logger logger = LoggerFactory.getLogger(EventDetailController.class);
+
     @Autowired
     private EventService eventService;
 
     @RequestMapping("/event/{id}")
     public ResponseEntity<Event> getEventDetail(@PathVariable("id") int eventId) {
+        logger.info("method getEventDetail from EventDetailController has been started. Param: id = " + eventId);
         Optional<Event> event = eventService.findById(eventId);
-        return event.isPresent() ? new ResponseEntity<>(event.get(), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        ResponseEntity<Event> resultResponseEntity = event.isPresent() ? new ResponseEntity<>(event.get(), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        logger.info("method getEventDetail from EventDetailController has finished. Result:"
+                + " Status code: " + resultResponseEntity.getStatusCode() +
+                " Body: event id = " + event.map(Event::getId).map(String::valueOf).orElse("null") +
+                "; event name = " + event.map(Event::getName).orElse("null"));
+        return resultResponseEntity;
     }
 }
