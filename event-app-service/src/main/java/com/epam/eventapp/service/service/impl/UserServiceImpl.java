4a2eb2692423e1961ccdaed0e16c6a455f4e60f5
Maxim Ivanov <Maxim_Ivanov2@epam.com>
2016-01-15 17:30:56 +0300

    EA-20 Minor fix in UserServiceImpl

--- event-app-service/src/main/java/com/epam/eventapp/service/service/impl/UserServiceImpl.java
+++ event-app-service/src/main/java/com/epam/eventapp/service/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.epam.eventapp.service.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
@@ -13,6 +14,7 @@ import java.util.Optional;
 /**
  * UserService implementation
  */
+@Service
 public class UserServiceImpl implements UserService {
     private static final Logger LOGGER = LoggerFactory.getLogger(EventServiceImpl.class);
 
