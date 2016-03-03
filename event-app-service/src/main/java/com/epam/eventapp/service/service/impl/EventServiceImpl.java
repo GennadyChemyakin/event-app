97cb151a71d4a1fc1057b84ef923c7f260231083
Maxim Ivanov <Maxim_Ivanov2@epam.com>
2016-03-03 15:06:33 +0300

    EA-57 Added @transactional to service

--- event-app-service/src/main/java/com/epam/eventapp/service/service/impl/EventServiceImpl.java
+++ event-app-service/src/main/java/com/epam/eventapp/service/service/impl/EventServiceImpl.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.Optional;
 /**
  * EventService implementation
  */
+@Transactional
 @Service("eventService")
 public class EventServiceImpl implements EventService {
 
