643f740d5f00a90a58565b7043c4bad2cd3052c0
Denys Iakibchuk <Denys_Iakibchuk@epam.com>
2016-03-11 16:29:31 +0300

    EA-40 added tests, properties changes
    EA-40 renamed method name

--- event-app-web/src/main/java/com/epam/eventappweb/controller/ImageController.java
+++ event-app-web/src/main/java/com/epam/eventappweb/controller/ImageController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -35,15 +36,16 @@ public class ImageController {
 
     /**
      * Method for saving user image into the catalog and in SEC_USER table in db
-     * @param userName - username of user the picture belongs to.
-     * @param multipartFile  - Multipart file to load
+     *
+     * @param userName      - username of user the picture belongs to.
+     * @param multipartFile - Multipart file to load
      */
     @RequestMapping(value = "/image/user/{userName}", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void handleImageUpload(@PathVariable String userName,
-                                  @RequestParam("users_photo") MultipartFile multipartFile) throws IOException {
+    public void saveUserImage(@PathVariable String userName,
+                              @RequestParam("users_photo") MultipartFile multipartFile) throws IOException {
 
-        LOGGER.info("handleImageUpload started. Params: username - {}, Mfile - {}", userName, multipartFile);
+        LOGGER.info("handleImageUpload started. Params: username: {}, multipartFile: {}", userName, multipartFile);
 
         String photoLink = imageService.saveUserImage(userName, multipartFile.getBytes());
         userService.updateUserPhotoByUsername(userName, photoLink);
@@ -53,18 +55,19 @@ public class ImageController {
 
     /**
      * Method for getting userImage
-     * @param userName
+     *
+     * @param username - name of the user
      * @return an image in bytes
      */
-    @RequestMapping(value = "/image/user/{userName}", method = RequestMethod.GET, produces = "image/png")
-    public byte[] getImage(@PathVariable String userName) {
+    @RequestMapping(value = "/image/user/{username}", method = RequestMethod.GET, produces = {"image/png", "image/jpg", "image/jpeg"})
+    public byte[] getUserImage(@PathVariable String username) {
 
-        LOGGER.info("getImage started. PathVariables: id - {}", userName);
+        LOGGER.info("getImage started. Params: username: {}", username);
 
-        String photoLink = userService.getUserByUsername(userName).getPhoto();
-        byte[] photo     = imageService.getUserPhoto(photoLink,userName);
+        String photoLink = userService.getUserByUsername(username).getPhoto();
+        byte[] photo = imageService.getUserPhoto(photoLink, username);
 
-        LOGGER.info("getImage finished. Return photo data size: {} bytes", photo.length);
+        LOGGER.info("getImage finished. Returns bytes: {}", photo.length);
         return photo;
 
     }
